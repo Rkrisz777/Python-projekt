75068e458b31c62d4ef134132c6dcc53f34d18e0
ReizingerKrisztofer <93581006+Rkrisz777@users.noreply.github.com>
2022-11-17 13:08:45 +0100

    frissítés

--- Python projekt/functions.py
+++ Python projekt/functions.py
@@ -2,15 +2,16 @@ from data import *
 from os import system
 from random import randint
 import time
+filename = 'adat.csv'
 
 def menu():
     system('cls')
     print('0 - Kilép')
-    print('1 - Valuták száma')
+    print('1 - Valuták listája')
     print('2 - Árfolyam')
     print('3 - Átváltás')
     print('4 - Új pénznem felvétele')
-    print('5 - Listázás')
+    print('5 - Keresés')
     print('6 - Törlés')
     return input('Kérem válasszon: ')
 
@@ -49,7 +50,58 @@ def penzValtas():
     print('Átváltva ennyi: ',ertek2 * int(mennyiseg1))
     time.sleep(2)
 
-
-   
-
+def ujPenzFelvetel():
+    system('cls')
+    print('Új pénznem feelvétele')
+    penz = input('Valuta: ')
+    ertek = float(input('Értéke: '))
+    penzek.append(penz)
+    ujertek.append(ertek)
+    saveToFile(penz, ertek)
+    print('Sikeres felvétel.')
+    input()
+def saveToFile(penz, ertek):
+    file = open(filename, 'a', encoding='utf-8')
     
+    file.write(f'{penz};{ertek}\n')
+
+    file.close
+def loadFromFile():
+    file = open(filename, 'r', encoding='utf-8')
+    for row in file:
+        splitted = row.strip().split(';')
+        penzek.append(splitted[0])
+        ujertek.append(float(splitted[1]))
+    file.close()
+
+def keresesAListában(needle):
+    for penz in penzek:
+        if penz == needle:
+            return 'Létezik'
+    return False
+    
+def penzTörles():
+    system('cls')
+    print('Valuta törölve:')
+    penz = input('Ki legyen törölve: ')
+    index = keresesAListában(penz)
+    penzek.pop(index)
+    ujertek.pop(index)
+    saveAll()
+    print(f'{penz} törölve.')
+    input()
+def saveAll():
+    file = open(filename, 'w', encoding='utf-8')
+    for index in range(len(penzek)):
+        file.write(f'{penzek[index]};{ujertek[index]}\n')
+
+    file.close
+def Kilep():
+    system('cls')
+    
+def Kilistazas():
+    system('cls')
+    print('Pénznemek listája:')
+    for i in range(len(penzek)):
+        print(f'\t {penzek[i]}')
+    input()
--- Python projekt/main.py
+++ Python projekt/main.py
@@ -2,11 +2,20 @@ from functions import *
 choice = ''
 while choice != '0':
     choice = menu()
+    if choice == '0':
+        Kilep()
     if choice == '1':
-        valutaSzam()
+        Kilistazas()
     elif choice == '2':
          arFolyam()
     elif choice == '3':
-        penzValtas()    
-
+        penzValtas()
+    elif choice == '4':
+        ujPenzFelvetel() 
+    elif choice == '5':   
+        penz = input('Adja meg a keresendő pénznemet: ')
+        print(keresesAListában(penz))
+        input()
+    elif choice == '6':
+        penzTörles()
         
